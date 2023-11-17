ffad62cb5ad115f629b05e6676481e3bae7b0dc5
FFS28 <benjaminvogelcottbus@gmail.com>
2023-11-17 01:48:52 -0500

    update language

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ const Footer = ({expanded}) => {
     return (
         <>
             {expanded && <div className='footer'>
-                <div className='footer-list'>Languages</div>
+                <div className='footer-list'>Language</div>
                 <div className='footer-list'>Get HELP</div>
                 <div className='footer-list'>EXIT</div>
             </div>}
